d3188e36dd04707171eaca0878ccc3fc3320c558
Nikita Danakari <nikitadanakari@MacBook-Air-Nikita.local>
2025-02-26 18:52:32 +0300

    add model gen and thesis draft

---
+++ model2d_gen.py
@@ -0,0 +1,266 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
+import math
+import os
+
+
+def model_generator(num):
+    def ragged_line(x):
+        return math.sin(x/100) 
+
+    if not os.path.exists(f'./dataset/models/model_{num}'):
+        os.makedirs(f'./dataset/models/model_{num}')
+
+    if not os.path.exists(f'./dataset/configs/config_{num}'):
+        os.makedirs(f'./dataset/configs/config_{num}')
+
+    # Параметры модели
+    x_size, y_size = 5000, 2000  # Размеры модели (длина и глубина)
+    H = 2000  # Глубина в метрах
+    num_layers = np.random.randint(3, 11)  # Случайное количество слоев
+
+    # Диапазоны параметров слоев
+    rho_min, rho_max = 2.0, 5.0  # г/см^3
+    vp_min, vp_max = 2.2, 5.0  # км/с
+    delimiter = np.random.uniform(2.0, 3.0)
+    multiplicator = np.random.uniform(1.1, 1.15)
+
+    # Генерация параметров слоев
+    layer_thicknesses = np.random.uniform(0.7 * H / num_layers, 1.4 * H / num_layers, num_layers)
+    layer_depths = np.cumsum(layer_thicknesses).astype(int)
+    layer_depths[layer_depths >= y_size] = y_size - 1  # Ограничение по глубине
+
+    rho = [np.random.uniform(rho_min, rho_min+1.5)]
+    vp = [rho[0] * multiplicator]
+    vs = [vp[0] / delimiter]
+
+    for i in range(1, num_layers):
+        temp_rho = rho[i-1] * np.random.uniform(1, 2.4 ** (1 / num_layers))
+        rho.append(temp_rho if temp_rho < rho_max else rho_max)
+        temp_vp = rho[i] * multiplicator
+        vp.append(temp_vp if temp_vp < vp_max else vp_max)
+        vs.append(vp[i] / delimiter)
+
+    # Создание 2D модели
+    model = np.zeros((x_size, y_size))
+    anomaly_type = np.random.choice(["ellipse", "mountain", "distorted_layers", "null"])
+    print(anomaly_type)
+
+    for i in range(num_layers):
+        start = layer_depths[i - 1] if i > 0 else 0
+        end = layer_depths[i]
+
+        for x in range(x_size):     
+            if start == 0:
+                model[x, start: int(end + 30 * ragged_line(x + 2))] = i + 1
+            elif num_layers - 1 == i:
+                model[x, int(start + 30 * ragged_line(x)):] = i + 1
+            else:      
+                model[x, int(start + 30 * ragged_line(x)): int(end + 30 * ragged_line(x + 2))] = i + 1
+
+    # Добавление аномалий
+    if anomaly_type == "ellipse":
+        cx, cy = np.random.randint(1000, 3500), np.random.randint(200, 1500) 
+        rx, ry = np.random.randint(50, 500), np.random.randint(60, 200) 
+
+        for x in range(x_size):
+            for y in range(y_size):
+                if ((x - cx) / rx) ** 2 + ((y - cy) / ry) ** 2 <= 1:
+                    model[x, y] = num_layers + 1
+
+    elif anomaly_type == "mountain":
+        x_points = np.linspace(2000, 3000, num=4)
+        y_points = np.random.uniform(0, 500, size=4)
+        y_points[0], y_points[-1] = 0, 0
+        spline = CubicSpline(x_points, y_points)
+
+        for x in range(x_size):
+            y_top = int(spline(x)) if 2000 <= x < 3000 else 0
+            model[x, int(y_size - y_top):] = num_layers + 1
+
+    elif anomaly_type == "distorted_layers":
+        x_points = np.linspace(0, 5000, num=3)
+        y_points = np.random.uniform(5, 1000, size=3)
+        y_points[0], y_points[-1] = 0, 1500
+        spline = CubicSpline(x_points, y_points)
+
+        for x in range(x_size):
+            y_top = int(spline(x) + x / 500) if x >= x_points[0] else 0
+            model[x, int((y_size - y_top) + ragged_line(x)):] = num_layers + 1
+    
+    else:
+        pass
+
+    # Визуализация распределений rho, vp и vs
+    def save_distribution(data, title, filename):
+        plt.figure(figsize=(8, 6))
+        plt.imshow(data.T, cmap='viridis', norm='log')
+        plt.axis('off')  # Убираем оси
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+        plt.close()
+    # Распределение rho
+    rho_model = np.zeros_like(model, dtype=float)
+    for i in range(num_layers + 1):
+        if i < len(rho):
+            rho_model[model == i + 1] = rho[i]
+        else:
+            rho_model[model == i + 1] = 4.8
+    print(num_layers)
+    rho_model.astype(np.float64).tofile(f'rho_{num}.bin')
+    save_distribution(rho_model, 'rho', f'./dataset/models/model_{num}/rho.png')
+
+    # Распределение vp
+    vp_model = np.zeros_like(model, dtype=float)
+    for i in range(num_layers + 1):
+        if i < len(vp):
+            vp_model[model == i + 1] = vp[i]
+        else:
+            vp_model[model == i + 1] = 4.6
+    vp_model.astype(np.float64).tofile(f'vp_{num}.bin')
+    save_distribution(vp_model, 'Vp', f'./dataset/models/model_{num}/vp.png')
+
+    # Распределение vs
+    vs_model = np.zeros_like(model, dtype=float)
+    for i in range(num_layers + 1):
+        if i < len(vs):
+            vs_model[model == i + 1] = vs[i]
+        else:
+            vs_model[model == i + 1] = 2.0 + ((vs[-1] - np.average(vs)) / (vp_max / delimiter))
+    vs_model.astype(np.float64).tofile(f'vs_{num}.bin')
+    save_distribution(vs_model, 'Vs', f'./dataset/models/model_{num}/vs.png')
+
+def config_generator(num1, num2):
+    x_coord = 2500 * num2
+    config = f'''
+
+        verbose = true
+
+        dt = 0.0002
+
+        steps = 5000
+
+
+        [grids]
+            [grid]
+                id = ore_body
+                [node]
+                    name = ElasticMetaNode2D
+                [/node]
+                [material_node]
+                    name = ElasticMaterialMetaNode
+                [/material_node]
+                [material]
+                    c1 = 1
+                    c2 = 1
+                    rho = 1
+                [/material]
+                [factory]
+                    name = RectGridFactory
+                    size = 500, 200
+                    origin = 0, 0
+                    spacing = 1.6, 1.6
+                [/factory]
+                [schema]
+                    name = ElasticMatRectSchema2DRusanov3
+                [/schema]
+                [fillers]
+                    [filler]
+                        name = RectNoReflectFiller
+                        axis = 0
+                        side = 0
+                    [/filler]
+                    [filler]
+                        name = RectNoReflectFiller
+                        axis = 0
+                        side = 1
+                    [/filler]
+                    [filler]
+                        name = RectNoReflectFiller
+                        axis = 1
+                        side = 0
+                    [/filler]
+                    [filler]
+                        name = RectNoReflectFiller
+                        axis = 1
+                        side = 1
+                    [/filler]
+                [/fillers]
+                [correctors]
+                            [corrector]
+                        name = ForceRectElasticBoundary2D
+                                axis = 1
+                                side = 1
+                    [/corrector]
+
+                    [corrector]
+                        name = PointSourceCorrector2D
+                        coords = {x_coord}, 100, 0.0
+                        compression = 1.0
+                        axis = 1
+                        eps = 2
+                        save = source_{num1}_{num2}.vtk
+                        gauss_w = 5
+                        [impulse]
+                            name = FileInterpolationImpulse
+                            [interpolator]
+                                name = PiceWiceInterpolator1D
+                                file = ricker_30.txt
+                            [/interpolator]
+                        [/impulse]
+                    [/corrector]
+                [/correctors]
+
+            [/grid]
+        [/grids]
+
+        [contacts]
+        [/contacts]
+
+        [initials]
+            [initial]
+                name = StructuredFileLoader
+                path = vp_{num1}.bin
+                value = c1
+                binary = true
+                order = 1
+            [/initial]
+            [initial]
+                name = StructuredFileLoader
+                path = vs_{num1}.bin
+                value = c2
+                binary = true
+                order = 2
+            [/initial]
+            [initial]
+                name = StructuredFileLoader
+                path = rho_{num1}.bin
+                value = rho
+                binary = true
+                order = 3
+            [/initial]
+        [/initials]
+
+        [savers]
+            [saver]
+                name = RectGridPointSaver
+                path = seismogramm_{num1}_{num2}.txt
+                order = 1
+                # save = 100
+                save = 100
+                params = vx, vy
+                norms = 0, 0
+                save_receivers_vtk = receivers_{num1}_{num2}.vtk
+                save_receivers_txt = receivers_{num1}_{num2}.txt
+            [/saver]
+        [/savers]
+        '''
+    with open(f'./dataset/configs/config_{num1}/config_{num1}_{num2}.txt', 'w') as f:
+            f.save(config)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        model_generator(i)
+        for j in range(3):
+            config_generator(i, j)
