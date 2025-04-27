32a7368938e19e8e97dde9554c1f4a56ba7db007
Alex Kuruts <alex2003684@gmail.com>
2025-04-28 01:04:30 +0300

    data loading optimization (~30x learning cycle speed boost)
    .txt files parsing is too slow, now we are:
    * Preprocessing all the data in Dataset class inicialization
    * Saving preprocessed data in .npy files for faster loading

--- Unet/data_utils.py
+++ Unet/data_utils.py
@@ -1,5 +1,6 @@
-import numpy as np
 import os
+
+import numpy as np
 
 
 def try_load(path):
@@ -11,7 +12,7 @@ def try_load(path):
 
 
 def correct_(paths):
-    """
+    """F
     проверка датасета на папки, где нехватает данных (например, недосчитаны сейсмограммы)
     """
     # эта операция достаточно долгая, по-этому если она нужна я проделываю ее 1 раз и сохраняю результат в csv
@@ -30,17 +31,20 @@ def load_model_paths(BASE_PATH="../../model_2d_faults_03_2025/dataset"):
     model_nums = [int(folder[7:]) for folder in model_folders]
 
     # Формируем пути для моделей (rho, vp, vs)
-    model_paths = np.array([
-        [os.path.join(model_base_path, folder, f"{prefix}_{num}.bin")
-        for folder, num in zip(model_folders, model_nums)]
-        for prefix in ["rho", "vp", "vs"]
-    ]).T
+    model_paths = np.array(
+        [
+            [
+                os.path.join(model_base_path, folder, f"{prefix}_{num}.bin")
+                for folder, num in zip(model_folders, model_nums)
+            ]
+            for prefix in ["rho", "vp", "vs"]
+        ]
+    ).T
 
     print(f"loaded {model_paths.shape} paths")
     print(f"example:\n{model_paths[0]}")
 
     return model_paths
-
 
 
 def load_seism_paths(BASE_PATH="../../model_2d_faults_03_2025/dataset"):
@@ -51,13 +55,15 @@ def load_seism_paths(BASE_PATH="../../model_2d_faults_03_2025/dataset"):
     model_folders = sorted(os.listdir(model_base_path))
     model_nums = [int(folder[7:]) for folder in model_folders]
 
-    seism_paths = np.array([
+    seism_paths = np.array(
         [
-            os.path.join(seism_base_path, folder, f"seismogram_{num}_{i}", "seismogram.txt")
-            for folder, num in zip(seism_folders, model_nums)
+            [
+                os.path.join(seism_base_path, folder, f"seismogram_{num}_{i}", "seismogram.txt")
+                for folder, num in zip(seism_folders, model_nums)
+            ]
+            for i in range(3)
         ]
-        for i in range(3)
-    ]).T
+    ).T
 
     print(f"loaded {seism_paths.shape} paths")
     print(f"example:\n{seism_paths[0]}")
@@ -72,14 +78,14 @@ def load_fault_paths(BASE_PATH="../../model_2d_faults_03_2025/dataset"):
     model_nums = [int(folder[7:]) for folder in model_folders]
 
     # Формируем пути для моделей (rho, vp, vs)
-    fault_paths = np.array([
-        os.path.join(model_base_path, folder, f"fault_map_{num}.bin")
-        for folder, num in zip(model_folders, model_nums)
-    ])
+    fault_paths = np.array(
+        [
+            os.path.join(model_base_path, folder, f"fault_map_{num}.bin")
+            for folder, num in zip(model_folders, model_nums)
+        ]
+    )
 
     print(f"loaded {fault_paths.shape} paths")
     print(f"example:\n{fault_paths[0]}")
 
     return fault_paths
-
-
--- Unet/dataloaders.py
+++ Unet/dataloaders.py
@@ -1,3 +1,5 @@
+import os
+
 import albumentations as A
 import cv2
 import numpy as np
@@ -9,12 +11,52 @@ from torch.utils.data import DataLoader, Dataset
 
 def load_seism(seism_paths, index):
 
-    seism_0 = np.loadtxt((seism_paths[index][0]), dtype="f")[:,1:]
-    seism_1 = np.loadtxt((seism_paths[index][1]), dtype="f")[:,1:]
-    seism_2 = np.loadtxt((seism_paths[index][2]), dtype="f")[:,1:]
+    seism_0 = np.loadtxt((seism_paths[index][0]), dtype="f")[:, 1:]
+    seism_1 = np.loadtxt((seism_paths[index][1]), dtype="f")[:, 1:]
+    seism_2 = np.loadtxt((seism_paths[index][2]), dtype="f")[:, 1:]
     seism_3ch = np.stack((seism_0, seism_1, seism_2), axis=-1)
 
     return seism_3ch
+
+
+def load_seism_from_npy(seism_paths, index):
+
+    original_path = seism_paths[index][0]
+    parent_dir = os.path.dirname(os.path.dirname(original_path))
+    filename = f"seism_3ch.npy"
+    save_path = os.path.join(parent_dir, filename)
+
+    seism_3ch = np.load(save_path)
+
+    return seism_3ch
+
+
+def bake_seisms_to_npy(seism_paths, force=False):
+    """
+    Txt-файлы долго парсятся.
+    Функция загружает данные сейсмограмм из Txt, предобрабатывает их, сохраняет в быстро загружающиеся npy файлы
+    Вызывается 1 раз при инициализации датасета
+
+    Причем, эта работа проделывается лишь однократно для 1 датасета,
+    потому что при повторном вызове (если не стоит force) она ее проделывать не будет
+    """
+    # проверяем, была ли выполнена эта работа до нас
+    parent_dir = os.path.dirname(os.path.dirname(seism_paths[-1][0]))
+    filename = f"seism_3ch.npy"
+    save_path = os.path.join(parent_dir, filename)
+    already_done = os.path.exists(save_path)
+
+    if (not already_done) or (force):
+        print("воркаем над сейсм")
+        for index in range(len(seism_paths)):
+            seism_3ch = load_seism(seism_paths, index)
+            seism_3ch = normalize_seism(seism_3ch)
+
+            parent_dir = os.path.dirname(os.path.dirname(seism_paths[index][0]))
+            filename = f"seism_3ch.npy"
+            save_path = os.path.join(parent_dir, filename)
+
+            np.save(save_path, seism_3ch)
 
 
 def load_geomodel(model_paths, index):
@@ -25,6 +67,42 @@ def load_geomodel(model_paths, index):
     geomodel_3ch = np.stack((model_0, model_1, model_2), axis=-1)
 
     return geomodel_3ch
+
+
+def load_geomodel_from_npy(model_paths, index):
+
+    original_path = model_paths[index][0]
+    dir = os.path.dirname(original_path)
+    filename = f"geomodel_3ch.npy"
+    save_path = os.path.join(dir, filename)
+
+    geomodel_3ch = np.load(save_path)
+
+    return geomodel_3ch
+
+
+def bake_geomodels_to_npy(geomodel_paths, force=False):
+    """
+    Аналогично bake_seisms_to_npy
+    """
+    # проверяем, была ли выполнена эта работа до нас
+    dir = os.path.dirname(geomodel_paths[-1][0])
+    filename = f"geomodel_3ch.npy"
+    save_path = os.path.join(dir, filename)
+    already_done = os.path.exists(save_path)
+
+    if (not already_done) or (force):
+        print("воркаем над геомоделями")
+        for index in range(len(geomodel_paths)):
+            geomodel_3ch = load_geomodel(geomodel_paths, index)
+            geomodel_3ch = normalize_geomodel(geomodel_3ch)
+
+            original_path = geomodel_paths[index][0]
+            dir = os.path.dirname(original_path)
+            filename = f"geomodel_3ch.npy"
+            save_path = os.path.join(dir, filename)
+
+            np.save(save_path, geomodel_3ch)
 
 
 def load_fault(fault_paths, index):
@@ -56,20 +134,20 @@ class Seism_3ch_Dataset(Dataset):
         self.geomodel_paths = geomodel_paths
         self.transform = transform
 
+        bake_seisms_to_npy(self.seism_paths)
+        bake_geomodels_to_npy(self.geomodel_paths)
+
     def __len__(self):
         return len(self.seism_paths)
 
     def __getitem__(self, index):
 
-        seism_3ch = load_seism(self.seism_paths, index)
-        geomodel_3ch = load_geomodel(self.geomodel_paths, index)
-
-        seism_3ch = normalize_seism(seism_3ch)
-        geomodel_3ch = normalize_geomodel(geomodel_3ch)
+        seism_3ch = load_seism_from_npy(self.seism_paths, index)
+        geomodel_3ch = load_geomodel_from_npy(self.geomodel_paths, index)
 
         if self.transform is not None:
             augmented = self.transform(image=geomodel_3ch, seism=seism_3ch)
-            geomodel_3ch = augmented['image']
-            seism_3ch = augmented['seism']
+            geomodel_3ch = augmented["image"]
+            seism_3ch = augmented["seism"]
 
         return seism_3ch, geomodel_3ch
